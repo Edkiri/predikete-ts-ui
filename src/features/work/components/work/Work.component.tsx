187bc2eb9cb194ca16d4ed49671eede126cbe62b
Eduardo Kiriakos <32148180+Edkiri@users.noreply.github.com>
2022-09-09 13:54:15 -0400

    dalete, secondaty btn created

--- src/features/work/components/work/Work.component.tsx
+++ src/features/work/components/work/Work.component.tsx
@@ -10,8 +10,13 @@ export function Work({ work }: WorkProps) {
   return (
     <div className="WorkContainer">
       <div className="TitleContainer">
-        <h3>{work.description}</h3>
-        <span className="WorkType">{work.type}</span>
+        <div className="TitleContainerLeft">
+          <h3>{work.description}</h3>
+          <span className="WorkType">{work.type}</span>
+        </div>
+        <button type="button" className="SecondaryButton">
+          Partidas
+        </button>
       </div>
       <span className="ClientName">{work.clientName}</span>
     </div>
